1b0888fcaa6f94e3eecf38828eefaec486a03391
Osman Özdemir <91970383+Osmannozdemir@users.noreply.github.com>
2022-06-22 00:33:24 +0300

    Kotlin İle Dört İşlem

---
+++ DortislemKotlin/app/src/main/java/com/osmanozdemir/dortislemkotlin/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.osmanozdemir.dortislemkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var sayi1 : Int? = null
+    var sayi2 : Int? = null
+    var sonuc : Int? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun topla(view : View){
+         sayi1 = ediitText.text.toString().toIntOrNull()
+        sayi2 = ediitText2.text.toString().toIntOrNull()
+
+        if(sayi1 == null || sayi2 == null){
+            textView.text = "sayı girmeyi unutmayınız"
+
+        }else {
+             sonuc = sayi1!! + sayi2!!
+            textView.text = "Sonuç: ${sonuc}"
+        }
+
+
+
+    }
+
+    fun cikar(view : View){
+        sayi1 = ediitText.text.toString().toIntOrNull()
+        sayi2 = ediitText2.text.toString().toIntOrNull()
+
+        if(sayi1 == null || sayi2 == null){
+            textView.text = "sayı girmeyi unutmayınız"
+
+        }else {
+            sonuc = sayi1!! - sayi2!!
+            textView.text = "Sonuç: ${sonuc}"
+        }
+
+
+
+
+    }
+
+    fun bol(view : View){
+
+        sayi1 = ediitText.text.toString().toIntOrNull()
+        sayi2 = ediitText2.text.toString().toIntOrNull()
+
+        if(sayi1 == null || sayi2 == null){
+            textView.text = "sayı girmeyi unutmayınız"
+
+        }else {
+            sonuc = sayi1!! / sayi2!!
+            textView.text = "Sonuç: ${sonuc}"
+        }
+
+
+
+
+    }
+
+    fun carp(view : View){
+
+        sayi1 = ediitText.text.toString().toIntOrNull()
+        sayi2 = ediitText2.text.toString().toIntOrNull()
+
+        if(sayi1 == null || sayi2 == null){
+            textView.text = "sayı girmeyi unutmayınız"
+
+        }else {
+            sonuc = sayi1!! * sayi2!!
+            textView.text = "Sonuç: ${sonuc}"
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
